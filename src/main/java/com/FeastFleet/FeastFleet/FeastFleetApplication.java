96e74f9e7e0ca770f1d54d577b1f28b2b71d9abd
unknown <daniel.jose1999@outlook.com>
2023-12-13 11:15:15 -0500

    Start project

---
+++ src/main/java/com/FeastFleet/FeastFleet/FeastFleetApplication.java
@@ -0,0 +1,13 @@
+package com.FeastFleet.FeastFleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeastFleetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeastFleetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FeastFleet/FeastFleet/FeastFleetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FeastFleet.FeastFleet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeastFleetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
