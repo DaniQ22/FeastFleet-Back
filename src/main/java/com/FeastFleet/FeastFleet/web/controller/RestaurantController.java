5db37cdb7d46ce236072ba2be242397e86b5dfd1
Daniel_Thevening <gonfidaniel@gmail.com>
2024-01-03 15:42:03 -0500

    security III

--- src/main/java/com/FeastFleet/FeastFleet/web/controller/RestaurantController.java
+++ src/main/java/com/FeastFleet/FeastFleet/web/controller/RestaurantController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/restaurant")
-@CrossOrigin(origins = "http://localhost:4200")
+
 public class RestaurantController {
 
   private final RestaurantServiceInter restaurantServiceInter;
